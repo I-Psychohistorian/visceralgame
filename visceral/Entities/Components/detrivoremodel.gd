062d2b23247f5074782f1197dba471fb894ca67f
I-Psychohistorian <92660661+I-Psychohistorian@users.noreply.github.com>
2023-01-22 22:58:15 -0800

    tail anim complete
    detri tail animations done

--- visceral/Entities/Components/detrivoremodel.gd
+++ visceral/Entities/Components/detrivoremodel.gd
@@ -1,11 +1,13 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+onready var Dbody = $BodyDark
+onready var lbody = $BodyLight
+onready var short_tail = $ShortTail
+onready var long_tail = $LongTail
+onready var seg_tail = $SegmentedTail
+onready var normal_wings = $normalwings
+onready var folded_wings = $foldedwings
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
