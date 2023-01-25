e89fab4fed08ab0f4b9fd30e742c3d7ee2a87d0e
I-Psychohistorian <92660661+I-Psychohistorian@users.noreply.github.com>
2023-01-24 19:30:27 -0800

    detrivore model anims and script
    Animations and script for detrivore bug are complete

--- visceral/Entities/Components/detrivoremodel.gd
+++ visceral/Entities/Components/detrivoremodel.gd
@@ -8,11 +8,122 @@ onready var long_tail = $LongTail
 onready var seg_tail = $SegmentedTail
 onready var normal_wings = $normalwings
 onready var folded_wings = $foldedwings
+onready var pilus = $matingpilus
+onready var side_eggs = $eggs
+
+var lightbody = false
+var wing_kinds = ['Normal','Bent','None']
+var wings = ''
+var tail_kinds = ['Short','Long','Segmented','None']
+var tail = ''
+
+var male = false
+var eggs = false
+
+
+var super = false
+
+#movement
+
+export var jumping = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	blank_models()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func blank_models():
+	Dbody.visible = false
+	lbody.visible = false
+	short_tail.visible = false
+	long_tail.visible = false
+	seg_tail.visible = false
+	normal_wings.visible = false
+	folded_wings.visible = false
+	pilus.visible = false
+	eggs.visible = false
+
+func set_phenotype():
+	#body
+	if lightbody == false:
+		Dbody.visible = true
+	elif lightbody == true:
+		lbody.visible = true
+	#tail
+	if tail == 'Short':
+		short_tail.visible = true
+	elif tail == 'Long':
+		long_tail.visible = true
+	elif tail == 'Segmented':
+		seg_tail.visible = true
+	elif tail == 'None':
+		pass
+	#wings
+	if wings == 'Normal':
+		normal_wings.visible = true
+	elif wings == 'Bent':
+		folded_wings.visible = true
+	elif wings == 'None':
+		pass
+	#super
+	if super == true:
+		folded_wings.visible = true
+		normal_wings.visible = true
+		Dbody.visible = true
+		lbody.visible = true
+		seg_tail.visible = true
+
+func toggle_male():
+	if male == false:
+		male = true
+		pilus.visible = true
+	elif male == true:
+		male = false
+		pilus.visible = false
+
+func toggle_pregnant():
+	if eggs == false:
+		eggs = true
+		side_eggs.visible = true
+	elif eggs == true:
+		eggs = false
+		side_eggs.visible = false
+
+func squash():
+	$TailAnim.play("RESET")
+	$WingAnim.play("RESET")
+	$WingAnim.play("dead")
+
+func crawl():
+	$TailAnim.play("RESET")
+	if super == true:
+		pass #super animations later
+	elif tail == 'Short':
+		$TailAnim.play("ShortCrawl")
+	elif tail == 'Long':
+		$TailAnim.play("LongCrawl")
+	elif tail == 'Segmented':
+		$TailAnim.play("SegCrawl")
+
+func idle():
+	$TailAnim.play("RESET")
+	$WingAnim.play("RESET")
+	if super == true:
+		pass #super animations later
+	elif wings == 'Normal':
+		$WingAnim.play("IdleWing")
+	elif wings == 'Bent':
+		$WingAnim.play("IdleBent")
+
+func fly():
+	$WingAnim.play("RESET")
+	if super == true:
+		pass #super animations later
+	elif wings == 'Normal':
+		$WingAnim.play("NormalFly")
+	elif wings == 'Bent':
+		$WingAnim.play("BentFly")
